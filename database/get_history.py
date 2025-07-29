f50a4f95ef688d96363a51ce36a9416cf7dbabf8
sachin <sachincodes1@gmail.com>
2025-07-29 11:28:33 +0000

    started with extracting history of stocks

---
+++ database/get_history.py
@@ -0,0 +1,47 @@
+import mysql.connector
+import json
+import requests
+
+cnx = mysql.connector.connect(user='root', password='n3u3da!',
+                              host='127.0.0.1',
+                              database='stocks_overflow')
+
+# check if the connection is successful
+if cnx.is_connected():
+    print("Connected to the database successfully.")
+    
+
+sample_symbol = 'cop'
+
+def fetch_json_data(url):
+    """
+    Fetch JSON data from a given URL.
+    
+    Args:
+        url (str): The URL to fetch the JSON data from.
+        
+    Returns:
+        dict: The JSON data as a dictionary.
+    """
+    response = requests.get(url)
+    response.raise_for_status()
+    print("Fetched stock data successfully.")
+    return response.json()
+
+def store_json_data(data, filename):
+    """
+    Store JSON data to a file.
+    
+    Args:
+        data (dict): The JSON data to store.
+        filename (str): The name of the file to store the data in.
+    """
+    
+    
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+    print(f"Data stored in {filename} successfully.")
+    
+def get_history(symbol):
+    
+    # m
