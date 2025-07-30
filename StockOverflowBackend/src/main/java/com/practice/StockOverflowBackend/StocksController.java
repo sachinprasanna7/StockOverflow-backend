293bf257fa6cf10237ecf7a659b1c661937afaa1
Prerak Varshney <varshneyprerak@gmail.com>
2025-07-30 17:58:59 +0000

    Coded with Primary key and GET POst Method Resolved

--- StockOverflowBackend/src/main/java/com/practice/StockOverflowBackend/StocksController.java
+++ StockOverflowBackend/src/main/java/com/practice/StockOverflowBackend/StocksController.java
@@ -1,8 +1,10 @@
 package com.practice.StockOverflowBackend;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 
@@ -15,12 +17,14 @@ public class StocksController {
 
     @PostMapping(path="/addStocks")
     public @ResponseBody String addStocks (@RequestBody Stocks stocks){
-//        Stocks stocks = new Stocks();
-//        stocks.setSymbol_id(symbol_id);
-//        stocks.setSymbol(symbol);
-//        stocks.setCompany_name(company_name);
+        if (stocksRepository.existsById(stocks.getSymbol_id())){
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    "Stock with ID: " +
+                    stocks.getSymbol_id() + "already exists"
+            );
+        }
         stocksRepository.save(stocks);
-//        System.out.println(company_name);
         return "Details got saved";
     }
 
