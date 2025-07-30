602fa37507fa6ea27292efcd88e80165c8b49ad0
Sruthi R <srspammails6@gmail.com>
2025-07-30 10:50:54 +0000

    New Project Structure

---
+++ StockOverflowBackend/src/main/java/com/practice/StockOverflowBackend/StockOverflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.practice.StockOverflowBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockOverflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockOverflowBackendApplication.class, args);
+	}
+
+}
---
+++ StockOverflowBackend/src/test/java/com/practice/StockOverflowBackend/StockOverflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.StockOverflowBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockOverflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
