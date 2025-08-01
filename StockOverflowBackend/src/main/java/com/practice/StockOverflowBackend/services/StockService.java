f46634d51e8dd5be1946f3a9751aa8dd0b3a35a4
Prerak Varshney <varshneyprerak@gmail.com>
2025-08-01 12:14:10 +0000

    Done with the required task in stocks table and added swagger ui dependency

--- StockOverflowBackend/src/main/java/com/practice/StockOverflowBackend/services/StockService.java
+++ StockOverflowBackend/src/main/java/com/practice/StockOverflowBackend/services/StockService.java
@@ -7,8 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.sql.ResultSet;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 @Service
 public class StockService {
@@ -17,18 +18,39 @@ public class StockService {
     private StocksRepository stocksRepository;
 
     public List<Stocks> getStocks() {
+
         return stocksRepository.findAll();
     }
 
-    public void addStocks(Stocks stocks) {
-        if (stocksRepository.existsById(stocks.getSymbol_id())){
-            throw new ResponseStatusException(
-                    HttpStatus.CONFLICT,
-                    "Stock with ID: " +
-                            stocks.getSymbol_id() + "already exists"
-            );
-        }
-        System.out.println(stocks);
-        stocksRepository.save(stocks);
+    public Stocks getStockById(int stock_id){
+        return stocksRepository.findById(stock_id)
+                .orElseThrow(()-> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Stock with ID: "+stock_id+" not found"
+                ));
     }
+
+    public List<Stocks> searchStocks(String query){
+        String lowerQuery = query.toLowerCase(Locale.ROOT);
+        List<Stocks> allStocks = stocksRepository.findAll();
+
+        return allStocks.stream().filter(
+                stock->
+                        stock.getSymbol().toLowerCase().contains(lowerQuery) ||
+                        stock.getCompanyName().toLowerCase().contains(lowerQuery)
+                )
+                .collect(Collectors.toList());
+    }
+
+//    public void addStocks(Stocks stocks) {
+//        if (stocksRepository.existsById(stocks.getSymbol_id())){
+//            throw new ResponseStatusException(
+//                    HttpStatus.CONFLICT,
+//                    "Stock with ID: " +
+//                            stocks.getSymbol_id() + "already exists"
+//            );
+//        }
+//        System.out.println(stocks);
+//        stocksRepository.save(stocks);
+//    }
 }
