e7fe91fa11d58bf669d0b8c51c90b53775aa3ed6
Khushi <khushimuddi10@gmail.com>
2025-08-03 05:47:53 +0000

    Added search function to OrderHistory

--- StockOverflowBackend/src/main/java/com/practice/StockOverflowBackend/repositories/OrderHistoryRepository.java
+++ StockOverflowBackend/src/main/java/com/practice/StockOverflowBackend/repositories/OrderHistoryRepository.java
@@ -2,9 +2,21 @@ package com.practice.StockOverflowBackend.repositories;
 
 import com.practice.StockOverflowBackend.entities.Order_History;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderHistoryRepository extends JpaRepository<Order_History, Integer> {
     // JpaRepository already provides methods like save, findById, findAll, deleteById
+
+    // Method 2: Custom JPQL query for partial search
+    @Query("""
+    SELECT o FROM Order_History o
+    WHERE LOWER(o.stock.companyName) LIKE LOWER(CONCAT('%', :keyword, '%'))
+       OR LOWER(o.stock.symbol) LIKE LOWER(CONCAT('%', :keyword, '%'))
+""")
+    List<Order_History> searchByStockNameOrSymbol(@Param("keyword") String keyword);
 }
